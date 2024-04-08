1be1bef31fba7ed1919f413d77c188f90f8580f7
Sergey Krivtsov <flashgangsta@gmail.com>
2024-04-08 17:55:52 +0200

    Routes list

---
+++ src/shared/ROUTES.ts
@@ -0,0 +1,14 @@
+export const enum ROUTES {
+  INDEX = "/",
+  ABOUT = "/about",
+  TEAM = "/about#team",
+  ROADMAP = "/about#roadmap",
+  TECHNOLOGY = "/technology",
+  NFT = "/technology#nft-profile",
+  PROTOCOL = "/technology#protocol",
+  BLOCKCHAIN = "/technology#blockchain",
+  INFRASTRUCTURE = "/infrastructure",
+  APP = "/infrastructure#app",
+  AD_MANAGER = "/infrastructure#advert-manager",
+  CONTACTS = "/contacts",
+}
