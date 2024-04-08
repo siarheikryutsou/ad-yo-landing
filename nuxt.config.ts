6f4a16510acac439648c88080197f54776b01bb2
Sergey Krivtsov <flashgangsta@gmail.com>
2024-04-08 14:35:30 +0200

    Configue project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: "en"
+            },
+        }
+    },
+
+    srcDir: "src",
+
+    components: [
+        "~/components",
+    ],
+
+    css: ["~/assets/css/main.css"],
+
+    postcss: {
+        plugins: {
+            'postcss-import': {},
+            'tailwindcss/nesting': {},
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    },
+
+
+    devServer: {
+        host: "0.0.0.0",
+        port: 3004
+    }
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+      "./src/components/**/*.vue",
+      ".src/layouts/**/*.vue",
+      ".src/pages/**/*.vue",
+      "./src/app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
