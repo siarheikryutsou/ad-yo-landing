764cd352d3e514b946dffd148f4c022c96dc48a3
Sergey Krivtsov <flashgangsta@gmail.com>
2024-05-01 15:48:39 +0200

    Elastic logo

--- src/components/ElasticLogo.vue
+++ src/components/ElasticLogo.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+withDefaults(defineProps<{part?: "full" | "ad" | "yo"}>(), {
+  part: "full"
+});
 </script>
 
 <template>
-  <div class="flex justify-center items-center gap-1 mt-6 mb-12">
-    <nuxt-icon name="intro_ad" filled />
+  <div class="flex justify-center items-center gap-1">
+    <nuxt-icon v-if="part !== 'yo'" name="intro_ad" filled />
     <div class="w-full h-[5px] bg-black mt-2"></div>
-    <nuxt-icon name="intro_yo" class="pt-[10px]" filled />
+    <nuxt-icon v-if="part !== 'ad'" name="intro_yo" class="pt-[10px]" filled />
   </div>
 </template>
