724214c5d155ac7699bd311a79e73b81776039d3
Sergey Krivtsov <flashgangsta@gmail.com>
2024-05-01 15:49:59 +0200

    set responsive paddings

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex justify-between items-center fixed w-full z-50">
-    <nuxt-link to="/" class="pl-6 md:pl-10" aria-label="Ad-Yo Logo">
+    <nuxt-link to="/" class="pl-6 md:pl-10 py-3" aria-label="Ad-Yo Logo">
       <nuxt-icon name="header_logo" class="animate-logo-to-white" />
     </nuxt-link>
     <Menu />
