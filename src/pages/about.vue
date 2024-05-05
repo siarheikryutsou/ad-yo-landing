a6dbc007bb18bd5d238226867882d8ca824e6652
Sergey Krivtsov <flashgangsta@gmail.com>
2024-05-05 21:15:03 +0200

    About layout

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,11 +2,12 @@
 
 useSeoMeta({
   title: "Scalable open-source platform based on blockchain | Ad-Yo",
-  description: "Learn about Ad-Yo blockchain protocol, ADYO token, meet the team and discover Ad-Yo's strategic roadmap.",
+  description: "Learn about Ad-Yo blockchain protocol, ADYO token, meet the team and discover Ad-Yo's strategic roadmap."
 });
 
 const roadmapContent = [
-  { title: "Roadmap",
+  {
+    title: "Roadmap",
     year: "2023",
     list: [
       "Blockchain testnet rollout",
@@ -14,7 +15,7 @@ const roadmapContent = [
       "Campaign manager",
       "Back-end architecture",
       "App prototype",
-      "Pre-seed round of investment — 150k EUR",
+      "Pre-seed round of investment — 150k EUR"
     ]
   },
 
@@ -28,7 +29,7 @@ const roadmapContent = [
       "Generative NFT launch",
       "Initial advertisers",
       "Presale and public sale of tokens (ICO)",
-      "Network decentralisation",
+      "Network decentralisation"
     ]
   },
 
@@ -40,7 +41,7 @@ const roadmapContent = [
       "Customers net growth",
       "Advertisers net growth",
       "Publishers net growth",
-      "Ad-Yo foundation investments",
+      "Ad-Yo foundation investments"
     ]
   }
 ];
@@ -77,21 +78,41 @@ const team = [
 </script>
 
 <template>
-  <section>
-    <h1>Digital advertising using blockchain technology</h1>
-    <p>The idea of Ad-Yo originated in 2021 and the development started in 2022.</p>
-    <p>The company is headquartered in Paris, France, and the entire team consists of 8 people.</p>
+  <section class="h-auto !min-h-0">
+    <div class="relative px-[30px]">
+      <div class="relative pt-[56.25%] -mx-[30px]">
+        <div
+          class="bg-about absolute overflow-hidden inset-0 w-full bg-no-repeat bg-[length:120%] bg-[left_top]">
+          <div class="wrapper flex w-full flex-col h-full">
+            <div class="flex w-full h-full justify-between">
+              <div class="flex items-end">
+                <h1 class="">Digital advertising<br /> using blockchain<br /> technology</h1>
+              </div>
+              <div class="info-block max-w-[300px] mt-10">
+                <p>The idea of Ad-Yo originated in 2021 and the development started in 2022.</p>
+                <p>The company is headquartered in Paris, France, and the entire team consists of 8 people. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-about">
+      <!--      <h1>Digital advertising using blockchain technology</h1>
+            <p>The idea of Ad-Yo originated in 2021 and the development started in 2022.</p>
+            <p>The company is headquartered in Paris, France, and the entire team consists of 8 people.</p>-->
+    </div>
   </section>
 
   <section class="wrapper !pt-28 !pb-40">
     <h1 class="text-center mb-28">Team</h1>
     <div class="flex gap-32">
       <div class="flex-1" v-for="person in team" :key="person.name">
-        <div class="h-64 w-full mb-28">
+        <div class="h-64 w-full mb-14">
           <img class="object-contain w-full h-full" :src="`/images/${person.img}`" :alt="`${person.name}'s photo`" />
         </div>
         <h3 class="font-bold">{{ person.name }}</h3>
-        <p class="mb-28">{{ person.position }}</p>
+        <p class="mb-14">{{ person.position }}</p>
         <p class="mb-10 font-bold">{{ person.description }}</p>
         <p>{{ person.experience }}</p>
       </div>
@@ -100,9 +121,9 @@ const team = [
 
   <section>
     <div class="wrapper !mb-0">
-      <h2 class="text-8xl mb-28">Roadmap</h2>
+      <h2 class="h1 mb-28">Roadmap</h2>
       <div class="flex">
-        <div v-for="point in roadmapContent" :key="point.year" class="flex-1 text-3xl">
+        <div v-for="point in roadmapContent" :key="point.year" class="flex-1 h2">
           <div>{{ point.year }}</div>
           <div class="w-px h-24 border border-black mt-7"></div>
         </div>
@@ -113,8 +134,8 @@ const team = [
     <div class="wrapper flex [&>*]:flex-1">
       <div v-for="point in roadmapContent" :key="point.title">
         <div class="w-px h-24 border border-black mb-7"></div>
-        <h3 class="text-3xl font-bold mb-16">{{ point.title }}</h3>
-        <ul class="list-disc text-2xl font-medium ml-8">
+        <h3 class="h2 mb-16">{{ point.title }}</h3>
+        <ul class="list-disc ml-8">
           <li v-for="item in point.list" :key="item">{{ item }}</li>
         </ul>
       </div>
